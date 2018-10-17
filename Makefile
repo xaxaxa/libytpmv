f01ab08b7b9c83b4a779927da1f58c50925a4095
xaxaxa <private0x01@gmail.com>
2018-10-17 02:21:32 +0100

    add test program for frame renderer

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
-LIBYTPMV=modparser.C audiorenderer.C samplecache.C mmutil.C -lSoundTouch  `pkg-config --cflags --libs gstreamer-1.0 gio-2.0`
+LIBYTPMV=modparser.C audiorenderer.C samplecache.C mmutil.C framerenderer.C -lGL -lGLEW -lEGL -lX11 -lgbm -lSoundTouch  `pkg-config --cflags --libs gstreamer-1.0 gio-2.0`
 
 test1:
 	g++ -o test1 test1.C modparser.C --std=c++0x -g3
@@ -7,3 +7,5 @@ test2:
 	g++ -o test2 test2.C $(LIBYTPMV) --std=c++0x -g3 -Wall
 test3:
 	g++ -o test3 test3.C $(LIBYTPMV) --std=c++0x -g3 -Wall
+test4:
+	g++ -o test4 test4.C $(LIBYTPMV) --std=c++0x -g3 -Wall
---
+++ test4.C
@@ -0,0 +1,43 @@
+#include "include/framerenderer.H"
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace std;
+using namespace ytpmv;
+int main(int argc, char** argv) {
+	
+	string shader =
+	"vec2 mypos = vec2(param(0), param(1));\n\
+	vec2 mysize = vec2(0.2,0.2);\n\
+	vec2 myend = mypos+mysize;\n\
+	vec2 relpos = (pos-mypos)/mysize;\n\
+	if(pos.x>=mypos.x && pos.y>=mypos.y \n\
+		&& pos.x<myend.x && pos.y<myend.y) \n\
+		return vec4(texture2D(image, relpos).rgb, 0.5);\n\
+	return vec4(0,0,0,0);\n";
+	
+	//texture2D(image, relpos).rgb
+	
+	string img1data = get_file_contents("fuck.data");
+	Image img1 = {480, 371, img1data};
+	
+	
+	FrameRenderer r(1920,1080);
+	r.setRenderers({shader}, 8, 8);
+	r.setEnabledRenderers({0,0,0});
+	//r.setTime(1.2);
+	
+	string imgdata;
+	
+	fprintf(stderr, "rendering\n");
+	for(int i=0;i<30*5;i++) {
+		r.setUserParams({{.7,.7}, {.35,.38}, {.57,.62}});
+		r.setImages({img1, img1, img1});
+		imgdata = r.render();
+	}
+	
+	uint32_t* dat = (uint32_t*)imgdata.data();
+	
+	write(1,imgdata.data(),imgdata.length());
+	return 0;
+}
