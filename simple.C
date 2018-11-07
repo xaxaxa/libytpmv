2ef086f068e0c75797f76d1a1def7c74b0c5f969
xaxaxa <private0x01@gmail.com>
2018-11-07 01:57:48 +0000

    add cmdline options

--- simple.C
+++ simple.C
@@ -223,10 +223,12 @@ namespace ytpmv {
 	
 	void parseOptions(int argc, char** argv) {
 		int opt;
-		while ((opt = getopt(argc, argv, "vq")) != -1) {
+		while ((opt = getopt(argc, argv, "vqs:")) != -1) {
 			if(opt == 'v') verbosity = 1;
 			else if(opt == 'q') verbosity = -1;
-			else goto print_usage;
+			else if(opt == 's') {
+				defaultSettings.skipToSeconds = atof(optarg);
+			} else goto print_usage;
 		}
 		if(optind < argc) {
 			if(strcmp(argv[optind], "play") == 0) {
