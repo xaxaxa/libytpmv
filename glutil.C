74e3d8147a6f2ea3f8f89530adc3a0f7d1ed5f6e
xaxaxa <private0x01@gmail.com>
2018-11-20 07:50:16 +0000

    set default texture mipmap interpolation settings

--- glutil.C
+++ glutil.C
@@ -150,6 +150,7 @@ namespace ytpmv {
 		}
 		glfwMakeContextCurrent(window);
 		initGL(false);
+		glEnable(GL_MULTISAMPLE_ARB);
 		
 		glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 		
@@ -172,7 +173,8 @@ namespace ytpmv {
 		glBindTexture(GL_TEXTURE_2D, texture);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -1.5);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
 		glGenerateMipmap(GL_TEXTURE_2D);
